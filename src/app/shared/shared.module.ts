eb84732ae3e486419ffaf78cd22634be00fdd625
leiton-cr <luis.leiton.cr@gmail.com>
2021-12-21 23:59:37 -0600

    Primeros pasos
    Generacion de estructura de carpetas y primeros componentes de modulo compartido

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { UserHeaderComponent } from './components/user-header/user-header.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    UserHeaderComponent,
+    MediaPlayerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SideBarComponent,
+    UserHeaderComponent,
+    MediaPlayerComponent
+  ]
+})
+export class SharedModule { }
